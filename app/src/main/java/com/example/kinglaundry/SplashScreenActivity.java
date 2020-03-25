805ec67a645404bda35cf8a7f9b22d2023b1559e
Ukhti Salsabila <ukhtisalsabilaa@gmail.com>
2020-03-25 09:54:26 +0700

    hello

---
+++ app/src/main/java/com/example/kinglaundry/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.kinglaundry;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread()
+        {
+            public void run(){
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
